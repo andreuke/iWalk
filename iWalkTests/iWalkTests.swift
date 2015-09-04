6c1ca862aaee9ae4e77204afb2a36008183795e3
andreuke <andreatello92@gmail.com>
2015-09-04 15:50:16 +0200

    Initial Commit

---
+++ iWalkTests/iWalkTests.swift
@@ -0,0 +1,36 @@
+//
+//  iWalkTests.swift
+//  iWalkTests
+//
+//  Created by Andrea Piscitello on 04/09/15.
+//  Copyright © 2015 Giadrea. All rights reserved.
+//
+
+import XCTest
+@testable import iWalk
+
+class iWalkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
