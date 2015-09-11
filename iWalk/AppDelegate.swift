a191b5edec37bf0dad589601b1bee09401a7cf5e
Giada Tacconelli <giadatacconelli@macbook-pro-di-giada.local>
2015-09-11 19:50:59 +0200

    cocoapods installed

--- iWalk/AppDelegate.swift
+++ iWalk/AppDelegate.swift
@@ -17,12 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        var pageControl = UIPageControl.appearance()
-        pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
-        pageControl.currentPageIndicatorTintColor = UIColor.blueColor()
-        pageControl.backgroundColor = UIColor.whiteColor()
-        
         return true
     }
 
