2ede0462ceeae2f4e199ff676ac37ba384f82be1
Giada Tacconelli <giadatacconelli@macbook-pro-di-giada.local>
2015-09-11 13:02:07 +0200

    charts page

---
+++ iWalk/BChartView-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  BChartView-Bridging-Header.h
+//  iWalk
+//
+//  Created by Giada Tacconelli on 08/09/15.
+//  Copyright © 2015 Giadrea. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import "JBChartView/JBChartView.h"
+#import "JBChartView/JBBarChartView.h"
+#import "JBChartView/JBLineChartView.h"
+
+#ifndef BChartView_Bridging_Header_h
+#define BChartView_Bridging_Header_h
+
+
+#endif /* BChartView_Bridging_Header_h */
